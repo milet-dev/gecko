9cc5bd9d3899ece96bacbea19b3a7ca76c104dbe
milet <milet@milet.org>
2023-03-14 00:50:01 +0300

    wip

--- src/git.rs
+++ src/git.rs
@@ -1,9 +1,26 @@
+use std::fmt::Display;
+
 use serde::Serialize;
 
 #[derive(Debug, Clone, Copy, Serialize)]
 pub enum BranchType {
     Local,
     Remote,
+}
+
+impl Display for BranchType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl BranchType {
+    pub fn as_str(&self) -> &str {
+        match self {
+            BranchType::Local => "Local",
+            BranchType::Remote => "Remote",
+        }
+    }
 }
 
 impl From<git2::BranchType> for BranchType {
@@ -16,9 +33,17 @@ impl From<git2::BranchType> for BranchType {
 }
 
 #[derive(Debug, Clone, Serialize)]
+pub struct Entry {
+    pub id: String,
+    pub name: String,
+    pub filemode: String,
+}
+
+#[derive(Debug, Clone, Serialize)]
 pub struct Branch {
     pub name: String,
     pub ty: BranchType,
+    pub entries: Vec<Entry>,
 }
 
 #[derive(Clone, Serialize)]
@@ -40,9 +65,22 @@ pub fn open(name: &str) -> anyhow::Result<Repository> {
         let Ok(Some(branch_name)) = branch.name() else {
             panic!()
         };
+
+        let mut entries = vec![];
+        let tree = repo.find_tree(branch.get().peel(git2::ObjectType::Tree)?.id())?;
+        tree.walk(git2::TreeWalkMode::PostOrder, |_, entry| {
+            entries.push(Entry {
+                id: entry.id().to_string(),
+                name: entry.name().unwrap().to_string(),
+                filemode: entry.filemode().to_string(),
+            });
+            git2::TreeWalkResult::Ok
+        })?;
+
         repository.branches.push(Branch {
             name: branch_name.to_owned(),
             ty: BranchType::from(branch_type),
+            entries,
         });
     }
 
--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,18 @@ mod git;
 use actix_web::{
     get, http::header::ContentType, web, App, HttpResponse, HttpServer, Responder, Result,
 };
+use askama::Template;
+use askama_actix::TemplateToResponse;
 
 #[get("/")]
 async fn index() -> impl Responder {
     "Hello, world!"
+}
+
+#[derive(Template)]
+#[template(path = "repository.html")]
+struct RepositoryTemplate<'a> {
+    repository: &'a git::Repository,
 }
 
 #[get("/repository/{name}")]
@@ -15,19 +23,27 @@ async fn repository(path: web::Path<String>) -> Result<impl Responder> {
 
     let repository = match git::open(&name) {
         Ok(repository) => repository,
-        Err(err) => {
-            let body = format!(r#"{{ "message": "{err:?}" }}"#);
+        Err(_err) => {
+            /* let body = format!(r#"{{ "message": "{err:?}" }}"#);
             let response = HttpResponse::NotFound()
                 .content_type(ContentType::json())
                 .body(body);
-            return Ok(response);
+            return Ok(response); */
+            return Ok(HttpResponse::NotFound()
+                .content_type(ContentType::plaintext())
+                .body("Not Found"));
         }
     };
 
-    let response = HttpResponse::Ok()
+    /* let response = HttpResponse::Ok()
         .content_type(ContentType::json())
         .body(serde_json::to_string(&repository)?);
-    Ok(response)
+    Ok(response) */
+
+    Ok(RepositoryTemplate {
+        repository: &repository,
+    }
+    .to_response())
 }
 
 #[actix_web::main]
