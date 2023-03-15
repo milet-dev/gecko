0eef6078d0a8c0690a0a6e935a894118a9b6651e
milet <milet@milet.org>
2023-03-15 23:24:06 +0300

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 mod git;
 mod model;
 
+use actix_files::Files;
+use actix_identity::{Identity, IdentityMiddleware};
+use actix_session::{config::PersistentSession, storage::CookieSessionStore, SessionMiddleware};
 use actix_web::{
-    get, http::header::ContentType, post, web, App, HttpResponse, HttpServer, Responder, Result,
+    cookie::{time::Duration, Key},
+    error, get,
+    http::{header::ContentType, StatusCode},
+    post, web, App, HttpMessage, HttpRequest, HttpResponse, HttpServer, Responder, Result,
 };
 use askama::Template;
 use askama_actix::TemplateToResponse;
@@ -14,8 +20,13 @@ use crate::model::User;
 const DATABASE_NAME: &str = "gecko";
 
 #[get("/")]
-async fn index() -> impl Responder {
-    "Hello, world!"
+async fn index(identity: Option<Identity>) -> Result<impl Responder> {
+    let id = match identity.map(|id| id.id()) {
+        None => "None".to_owned(),
+        Some(Ok(id)) => id,
+        Some(Err(err)) => return Err(error::ErrorInternalServerError(err)),
+    };
+    Ok(format!("{id}"))
 }
 
 #[derive(Template)]
@@ -31,21 +42,11 @@ async fn repository(path: web::Path<String>) -> Result<impl Responder> {
     let repository = match git::open(&name) {
         Ok(repository) => repository,
         Err(_err) => {
-            /* let body = format!(r#"{{ "message": "{err:?}" }}"#);
-            let response = HttpResponse::NotFound()
-                .content_type(ContentType::json())
-                .body(body);
-            return Ok(response); */
             return Ok(HttpResponse::NotFound()
                 .content_type(ContentType::plaintext())
                 .body("Not Found"));
         }
     };
-
-    /* let response = HttpResponse::Ok()
-        .content_type(ContentType::json())
-        .body(serde_json::to_string(&repository)?);
-    Ok(response) */
 
     Ok(RepositoryTemplate {
         repository: &repository,
@@ -68,6 +69,7 @@ async fn signup() -> impl Responder {
 pub struct SignupFormParams {
     email: String,
     username: String,
+    password: String,
 }
 
 #[post("/signup")]
@@ -79,21 +81,71 @@ async fn add_user(
     let user = User {
         email: params.email.clone(),
         username: params.username.clone(),
+        password: params.password.clone(),
     };
     _ = collection.insert_one(&user, None).await;
     Ok("ok")
 }
 
+#[derive(Template)]
+#[template(path = "login.html")]
+struct LoginTemplate<'a> {
+    title: &'a str,
+}
+
+#[get("/login")]
+async fn login() -> impl Responder {
+    LoginTemplate { title: "login" }.to_response()
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct LoginFormParams {
+    username: String,
+    password: String,
+}
+
+#[post("/login")]
+async fn login_(
+    client: web::Data<Client>,
+    req: HttpRequest,
+    params: web::Form<LoginFormParams>,
+) -> impl Responder {
+    let collection = client.database(DATABASE_NAME).collection::<User>("users");
+    let filter =
+        bson::doc! { "username": params.username.clone(), "password": params.password.clone() };
+    let Ok(Some(user)) = collection.find_one(filter, None).await else {
+        return web::Redirect::to("/").using_status_code(StatusCode::NOT_FOUND)
+    };
+
+    Identity::login(&req.extensions(), user.username).unwrap();
+    web::Redirect::to("/").using_status_code(StatusCode::FOUND)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let secret_key = Key::generate();
+
     let client = Client::with_uri_str("mongodb://localhost:27017")
         .await
         .unwrap();
+
     HttpServer::new(move || {
         App::new()
+            .wrap(IdentityMiddleware::default())
+            .wrap(
+                SessionMiddleware::builder(CookieSessionStore::default(), secret_key.clone())
+                    .cookie_secure(false)
+                    .session_lifecycle(
+                        PersistentSession::default().session_ttl(Duration::minutes(1)),
+                    )
+                    .build(),
+            )
             .app_data(web::Data::new(client.clone()))
+            .service(Files::new("/static", "static"))
             .service(signup)
             .service(add_user)
+            .service(login)
+            .service(login_)
             .service(index)
             .service(repository)
     })
--- src/model.rs
+++ src/model.rs
@@ -2,6 +2,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct User {
-    pub username: String,
     pub email: String,
+    pub username: String,
+    pub password: String,
 }
