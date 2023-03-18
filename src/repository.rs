712be6c2f59943df8f490518a28a19c095c71d37
milet <milet@milet.org>
2023-03-18 15:17:20 +0300

    wip

--- src/repository.rs
+++ src/repository.rs
@@ -7,15 +7,17 @@ use std::path::Path;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Kind {
-    Dir,
+    Tree,
     File,
+    Submodule,
 }
 
 impl Kind {
     fn as_str(&self) -> &str {
         match self {
-            Kind::Dir => "Dir",
+            Kind::Tree => "Tree",
             Kind::File => "Kind",
+            Kind::Submodule => "Submodule",
         }
     }
 }
@@ -47,6 +49,7 @@ struct RepositoryTemplate<'a> {
     user: &'a Option<User>,
     entries: &'a [Entry],
     commit: Commit,
+    readme: Option<(String, String)>,
 }
 
 #[get("/repository/{name}")]
@@ -84,14 +87,30 @@ pub async fn index(
         },
     };
     let commit_tree = commit.tree().unwrap();
+
+    let mut readme: Option<(String, String)> = None;
     let mut entries = vec![];
     for entry in commit_tree.iter() {
         let entry_name = entry.name().unwrap();
+        if entry.kind() == Some(git2::ObjectType::Blob) {
+            if entry_name.starts_with("README") {
+                let blob = repo.find_blob(entry.id()).unwrap();
+                let content = String::from_utf8_lossy(blob.content());
+                readme = Some((
+                    entry_name.to_owned(),
+                    markdown::to_html_with_options(&content, &markdown::Options::gfm()).unwrap(),
+                ));
+            }
+        }
 
-        let entry_kind = match entry.kind().unwrap() {
-            git2::ObjectType::Tree => Kind::Dir,
+        let mut entry_kind = match entry.kind().unwrap() {
+            git2::ObjectType::Tree => Kind::Tree,
             _ => Kind::File,
         };
+
+        if repo.find_submodule(entry_name).is_ok() {
+            entry_kind = Kind::Submodule;
+        }
 
         entries.push(Entry {
             name: entry_name.to_string(),
@@ -99,7 +118,7 @@ pub async fn index(
         });
     }
 
-    entries.sort_by_key(|k| k.kind == Kind::File);
+    entries.sort_by_key(|e| e.kind == Kind::File);
 
     Ok(RepositoryTemplate {
         name: &name,
@@ -107,6 +126,7 @@ pub async fn index(
         user: &user,
         entries: &entries,
         commit: commit_,
+        readme,
     }
     .to_response())
 }
@@ -120,6 +140,7 @@ struct TreeTemplate<'a> {
     name: &'a str,
     branch: &'a str,
     tail: &'a str,
+    readme: Option<(String, String)>,
 }
 
 #[derive(Template)]
@@ -129,8 +150,9 @@ struct FileTemplate<'a> {
     branch: &'a str,
     tail: &'a str,
     user: &'a Option<User>,
+    blob_name: &'a str,
     content: &'a str,
-    size: String,
+    size: &'a str,
 }
 
 #[get("tree/{branch}/{tail}*")]
@@ -157,17 +179,31 @@ pub async fn tree(
     let commit = head.peel_to_commit().unwrap();
     let tree_entry = commit.tree().unwrap().get_path(Path::new(&tail)).unwrap();
     let object = tree_entry.to_object(&repo).unwrap();
+
     if let Some(blob) = object.as_blob() {
+        let blob_name = tail.split('/').last().unwrap();
+
         let content = String::from_utf8_lossy(blob.content());
         let size = humansize::format_size(blob.size(), humansize::DECIMAL.decimal_places(0));
+
+        let content = if blob_name.ends_with(".md") || blob_name.ends_with(".markdown") {
+            markdown::to_html_with_options(&content, &markdown::Options::gfm()).unwrap()
+        } else {
+            let mut buffer = String::new();
+            buffer.push_str("<pre>");
+            buffer.push_str(&content);
+            buffer.push_str("</pre>");
+            buffer
+        };
 
         return Ok(FileTemplate {
             name: &name,
             branch: &branch,
             tail: &tail,
             user: &user,
+            blob_name,
             content: &content,
-            size,
+            size: &size,
         }
         .to_response());
     }
@@ -185,12 +221,32 @@ pub async fn tree(
             email: author_email,
         },
     };
-
+    let mut readme: Option<(String, String)> = None;
     let mut entries = vec![];
     for entry in tree.iter() {
         let entry_name = entry.name().unwrap();
+
+        if entry.kind() == Some(git2::ObjectType::Blob) {
+            let blob_path = Path::new(entry_name);
+
+            if let Some(stem) = blob_path.file_stem() {
+                let stem = stem.to_string_lossy();
+
+                if let Some(ext) = blob_path.extension() {
+                    if stem == "README" && (ext == "md" || ext == "markdown") {
+                        let blob = repo.find_blob(entry.id()).unwrap();
+                        let content = String::from_utf8_lossy(blob.content());
+                        let output =
+                            markdown::to_html_with_options(&content, &markdown::Options::gfm())
+                                .unwrap();
+                        readme = Some((stem.into_owned(), output));
+                    }
+                }
+            }
+        }
+
         let entry_kind = match entry.kind().unwrap() {
-            git2::ObjectType::Tree => Kind::Dir,
+            git2::ObjectType::Tree => Kind::Tree,
             _ => Kind::File,
         };
 
@@ -200,7 +256,7 @@ pub async fn tree(
         });
     }
 
-    entries.sort_by_key(|k| k.kind == Kind::File);
+    entries.sort_by_key(|e| e.kind == Kind::File);
 
     Ok(TreeTemplate {
         user: &user,
@@ -209,6 +265,7 @@ pub async fn tree(
         name: &name,
         branch: &branch,
         tail: &tail,
+        readme,
     }
     .to_response())
 }
