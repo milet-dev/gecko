1d157d99bdb9b8f5422311ffc196d1c6e227a7cd
milet <milet@milet.org>
2023-07-03 11:03:01 +0300

    Improvements

--- src/time_utils.rs
+++ src/time_utils.rs
@@ -1,8 +1,8 @@
 use time::{format_description, OffsetDateTime, UtcOffset};
 
-pub fn to_relative_time(seconds: i64) -> String {
+pub fn to_relative_time(input: i64) -> String {
     let now = time::OffsetDateTime::now_utc().unix_timestamp();
-    let seconds = now - seconds;
+    let seconds = now - input;
     if seconds <= 10 {
         return "now".to_string();
     } else if seconds <= 59 {
@@ -18,13 +18,35 @@ pub fn to_relative_time(seconds: i64) -> String {
     }
     let days = hours / 24;
     if days <= 30 {
+        if days == 1 {
+            return "1 day ago".to_owned();
+        }
+        let weeks = days / 7;
+        if weeks >= 1 && weeks <= 3 {
+            if weeks == 1 {
+                return "1 week ago".to_owned();
+            }
+            return format!("{weeks} weeks ago");
+        } else if weeks >= 4 {
+            return "1 month ago".to_owned();
+        }
         return format!("{days} days ago");
     }
+
     let months = days / 30;
-    if months <= 12 {
+    if months <= 11 {
         return format!("{months} months ago");
     }
-    format!("{seconds}")
+
+    /* let years = months / 12;
+    if years == 1 {
+        return "1 year ago".to_owned();
+    } else {
+        return format!("{years} years ago");
+    } */
+
+    let time = OffsetDateTime::from_unix_timestamp(input).unwrap();
+    to_datetime_format(time, None, "[month repr:short] [day], [year]")
 }
 
 pub fn to_datetime(time: OffsetDateTime, offset: Option<i32>) -> String {
